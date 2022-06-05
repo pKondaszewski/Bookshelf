9551595a747a3829d75fed01e0dff97c3b6c5311
PrzemekGL <p.kondaszewski@globallogic.com>
2022-06-05 21:36:38 +0200

    log unit test, @autowired client

--- src/main/java/com/globallogic/bookshelf/service/BorrowService.java
+++ src/main/java/com/globallogic/bookshelf/service/BorrowService.java
@@ -14,6 +14,7 @@ import com.globallogic.bookshelf.utils.StringRepresentation;
 import com.globallogic.bookshelf.utils.UserHistory;
 import com.globallogic.bookshelf.utils.Verification;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -35,6 +36,7 @@ import java.util.Optional;
 @EnableFeignClients
 public class BorrowService {
 
+    @Autowired
     protected ShelfUserFeignClient shelfUserFeignClient;
     protected BorrowRepository borrowRepository;
     protected BookRepository bookRepository;
---
+++ src/test/java/com/globallogic/bookshelf/config/LogSchedulingConfigTest.java
@@ -0,0 +1,38 @@
+package com.globallogic.bookshelf.config;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+import com.globallogic.bookshelf.service.LogService;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ContextConfiguration(classes = {LogSchedulingConfig.class})
+@ExtendWith(SpringExtension.class)
+class LogSchedulingConfigTest {
+
+    @Autowired
+    private LogSchedulingConfig logSchedulingConfig;
+
+    @MockBean
+    private LogService logService;
+
+    @Test
+    void configureTasksTest() {
+        when(logService.getIntervalTime()).thenReturn(42);
+        doNothing().when(logService).handleFile();
+
+        ScheduledTaskRegistrar scheduledTaskRegistrar = new ScheduledTaskRegistrar();
+        logSchedulingConfig.configureTasks(scheduledTaskRegistrar);
+
+        assertTrue(scheduledTaskRegistrar.hasTasks());
+    }
+}
+
