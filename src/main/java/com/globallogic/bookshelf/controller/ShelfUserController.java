b3b4770b548124eea8bead0e28bf0168515cf067
bartlomiej.chojnacki <bartlomiej.chojnacki@globallogic.com>
2022-06-08 12:53:40 +0200

    Update javadoc

--- src/main/java/com/globallogic/bookshelf/controller/ShelfUserController.java
+++ src/main/java/com/globallogic/bookshelf/controller/ShelfUserController.java
@@ -9,13 +9,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+/**
+ * Client-server communication class that's processes /shelfUser requests
+ *
+ * @author Bartlomiej Chojnacki
+ * @author Przemyslaw Kondaszewski
+ * @since 1.0
+**/
+
+ @RestController
 @RequestMapping("/shelfUser")
 @RequiredArgsConstructor
 public class ShelfUserController implements ShelfUserFeignClient{
 
     private final ShelfUserFeignClient userFeignClient;
 
+    /**
+     * POST Request to create a User
+     *
+     * @param firstName
+     * @param lastName
+     * @param status
+     * @return ResponseEntity that contains information about created user.
+     */
     @Override
     public ResponseEntity<String> userAdd(@RequestParam String firstName
             , @RequestParam String lastName
@@ -26,6 +42,13 @@ public class ShelfUserController implements ShelfUserFeignClient{
         return new ResponseEntity<>(String.format("User %s %s created", firstName, lastName), HttpStatus.CREATED);
     }
 
+    /**
+     * GET Request to receive string that contains user status information.
+     *
+     * @param firstName
+     * @param lastName
+     * @return String that contains user status information.
+     */
     @Override
     public ResponseEntity<String> getUserStatus(@RequestParam String firstName,@RequestParam String lastName){
         String status = Status.NOT_PRESENT.name();
